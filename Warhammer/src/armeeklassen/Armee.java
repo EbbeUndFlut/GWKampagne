1094ebce95d3470b2a3f50b173f7b5f7de60f653
RamRod <ex@example.org>
2016-04-05 22:46:59 +0200

    Erste Funktionalitäten erstellt Es wird noch ein vordefiniertes Object der Armee.java Klasse erstellt. Es können die HQ und Standart Einheiten hinnzugefügt werden und über einen Undo Button kann immer die letzte Einheit wieder entfernt werden. Der Undo Button hat in seinem .fxml Dokument noch ein Tooltip bekommen.

--- Warhammer/src/armeeklassen/Armee.java
+++ Warhammer/src/armeeklassen/Armee.java
@@ -15,12 +15,35 @@ public class Armee {
     
     private String name;
     private int punkte;
+    private int kosten;
     
-    ArrayList<Einheiten> Einheiten;
+    
+    public ArrayList<Einheiten> Einheiten;
     
     public Armee(String n, int points){
         name = n;
         punkte = points;
         Einheiten = new ArrayList<Einheiten>();
+    }
+    
+    
+    public int getPunkte(){
+        return punkte;
+    }
+    public void setPunkte(int points){
+        punkte = points;
+    }
+    
+    public int punkteRechner(){
+        int zähler = Einheiten.size();
+        Einheiten heit;
+        kosten = punkte;
+        
+        for(int i = 0; i < zähler;i++){
+            
+            heit = Einheiten.get(i);
+            this.kosten-= heit.getPunkte();
+        }
+        return this.kosten;
     }
 }
